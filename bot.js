b87e79fa7ee0e7bd15069dcf79f7f369d361786d
Arnold Lee <arnold.lee.wt@gmail.com>
2018-01-17 13:06:36 +0800

    first commit

--- bot.js
+++ bot.js
@@ -1,113 +1,135 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+// var Botkit = require('./lib/Botkit.js');
+var Botkit = require('botkit');
 
-
-This is a sample Cisco Spark bot built with Botkit.
-
-# RUN THE BOT:
-  Follow the instructions here to set up your Cisco Spark bot:
-    -> https://developer.ciscospark.com/bots.html
-  Run your bot from the command line:
-    access_token=<MY BOT ACCESS TOKEN> public_address=<MY PUBLIC HTTPS URL> node bot.js
-
-
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+const https = require('https');
 var env = require('node-env-file');
 env(__dirname + '/.env');
 
-
-if (!process.env.access_token) {
-    console.log('Error: Specify a Cisco Spark access_token in environment.');
-    usage_tip();
-    process.exit(1);
-}
-
-if (!process.env.public_address) {
-    console.log('Error: Specify an SSL-enabled URL as this bot\'s public_address in environment.');
-    usage_tip();
-    process.exit(1);
-}
-
-var Botkit = require('botkit');
-var debug = require('debug')('botkit:main');
-
-// Create the Botkit controller, which controls all instances of the bot.
 var controller = Botkit.sparkbot({
-    // debug: true,
-    // limit_to_domain: ['mycompany.com'],
-    // limit_to_org: 'my_cisco_org_id',
+    debug: true,
+    log: true,
     public_address: process.env.public_address,
     ciscospark_access_token: process.env.access_token,
-    studio_token: process.env.studio_token, // get one from studio.botkit.ai to enable content management, stats, message console and more
-    secret: process.env.secret, // this is an RECOMMENDED but optional setting that enables validation of incoming webhooks
-    webhook_name: 'Cisco Spark bot created with Botkit, override me before going to production',
-    studio_command_uri: process.env.studio_command_uri,
-});
-
-// Set up an Express-powered webserver to expose oauth and webhook endpoints
-var webserver = require(__dirname + '/components/express_webserver.js')(controller);
-
-// Tell Cisco Spark to start sending events to this application
-require(__dirname + '/components/subscribe_events.js')(controller);
-
-// Load in some helpers that make running Botkit on Glitch.com better
-require(__dirname + '/components/plugin_glitch.js')(controller);
-
-// enable advanced botkit studio metrics
-require('botkit-studio-metrics')(controller);
-
-var normalizedPath = require("path").join(__dirname, "skills");
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  require("./skills/" + file)(controller);
+    secret: process.env.secret
 });
 
 
-// This captures and evaluates any message sent to the bot as a DM
-// or sent to the bot in the form "@bot message" and passes it to
-// Botkit Studio to evaluate for trigger words and patterns.
-// If a trigger is matched, the conversation will automatically fire!
-// You can tie into the execution of the script using the functions
-// controller.studio.before, controller.studio.after and controller.studio.validate
-if (process.env.studio_token) {
-    controller.on('direct_message,direct_mention', function(bot, message) {
-        if (message.text) {
-            controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
-                if (!convo) {
-                    // no trigger was matched
-                    // If you want your bot to respond to every message,
-                    // define a 'fallback' script in Botkit Studio
-                    // and uncomment the line below.
-                    controller.studio.run(bot, 'fallback', message.user, message.channel, message);
-                } else {
-                    // set variables here that are needed for EVERY script
-                    // use controller.studio.before('script') to set variables specific to a script
-                    convo.setVar('current_time', new Date());
-                }
-            }).catch(function(err) {
-                if (err) {
-                    bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-                    debug('Botkit Studio: ', err);
-                }
-            });
-        }
+var bot = controller.spawn({
+});
+
+controller.setupWebserver(process.env.PORT || 3000, function (err, webserver) {
+    controller.createWebhookEndpoints(webserver, bot, function () {
+        console.log("SPARK: Webhooks set up!");
     });
-} else {
-    console.log('~~~~~~~~~~');
-    console.log('NOTE: Botkit Studio functionality has not been enabled');
-    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
-}
+});
 
-function usage_tip() {
-    console.log('~~~~~~~~~~');
-    console.log('Botkit Studio Starter Kit');
-    console.log('Execute your bot application like this:');
-    console.log('access_token=<MY ACCESS TOKEN> public_address=<https://mybotapp/> node bot.js');
-    console.log('Get Cisco Spark token here: https://developer.ciscospark.com/apps.html')
-    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
-    console.log('~~~~~~~~~~');
-}
+controller.hears(['hello', 'hey', 'hi', 'aloha'], 'direct_message,direct_mention', function (bot, message) {
+    var message_options = [
+        "Hello!",
+        "Hello <@personEmail:" + message.user + ">!",
+        "Yes, I'm listening...",
+        "Hi! How can I help?",
+        "Hey, what's up!",
+        "Yes, tell me! What are you looking for?",
+        "What's up?",
+        "Yuhoo!",
+        "你好！"
+    ]
+    var random_index = Math.floor(Math.random() * message_options.length)
+    var chosen_message = message_options[random_index]
+
+    bot.reply(message, chosen_message)
+});
+
+controller.hears('bam', 'direct_message,direct_mention', function (bot, message) {
+    var message_options = [
+        "Did I just hear a bam? BAM!!!!",
+        "Woah! You like bam? Me too!",
+        "You BAM! I BAM! WE BAM!"
+    ]
+    var random_index = Math.floor(Math.random() * message_options.length)
+    var chosen_message = message_options[random_index]
+
+    bot.reply(message, chosen_message)
+});
+
+controller.on('direct_mention', function (bot, message) {
+    bot.reply(message, 'You mentioned me and said, "' + message.text + '"');
+});
+
+controller.on('direct_message', function (bot, message) {
+    bot.reply(message, 'I got your private message. You said, "' + message.text + '"');
+});
+
+controller.on('user_space_join', function (bot, message) {
+    bot.reply(message, 'Welcome <@personEmail:' + message.user + '>!');
+});
+
+controller.hears('is Sing cool?', 'direct_message,direct_mention', function (bot, message) {
+    bot.reply(message, "no");
+});
+
+controller.hears('translate', 'direct_message,direct_mention', function (bot, message) {
+
+    console.log("running.......................");
+    https.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180117T004805Z.b129a8b3ea79d22f.b7a43b194303543b45ded23a0b6890c124dc205e&text=hello my friend&lang=zh', (resp) => {
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            var toJson = JSON.parse(data);
+            console.log("data: " + toJson.text);
+            // console.log(JSON.parse(data).explanation);
+            bot.reply(message, JSON.stringify(toJson.text[0]));
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+
+});
+
+
+controller.hears('baba', 'direct_message,direct_mention', function (bot, message) {
+
+    console.log("running2.......................");
+
+    const options = {
+        hostname: 'https://translate.yandex.net',
+        path: '/api/v1.5/tr.json/translate?key=trnsl.1.1.20180117T004805Z.b129a8b3ea79d22f.b7a43b194303543b45ded23a0b6890c124dc205e&text=hello&lang=zh',
+        method: 'GET'
+    };
+
+    const req = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            var toJson = JSON.parse(data);
+            console.log("data: " + toJson.text);
+            // console.log(JSON.parse(data).explanation);
+            bot.reply(message, JSON.stringify(toJson.text[0]));
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
+    req.end();
+
+
+
+});
