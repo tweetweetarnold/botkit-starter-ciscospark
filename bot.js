2c08e20138fb3097ea033ae317d78afd9660b2e4
tweetweetarnold <arnold.lee.wt@gmail.com>
2018-01-20 17:03:45 +0800

    added thomson ngo seal of approval credit

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ controller.setupWebserver(process.env.PORT || 3000, function (err, webserver) {
 });
 
 controller.hears('help', 'direct_message,direct_mention', function (bot, message) {
-    bot.reply(message, 'Hi, ' + help_msg);
+    bot.reply(message, 'Hi, ' + intro_msg);
 });
 
 controller.on('direct_mention', function (bot, message) {
@@ -36,7 +36,7 @@ controller.on('direct_message', function (bot, message) {
 });
 
 controller.on('user_space_join', function (bot, message) {
-    bot.reply(message, 'Welcome <@personEmail:' + message.user + '>! ' + help_msg);
+    bot.reply(message, 'Welcome <@personEmail:' + message.user + '>! ' + intro_msg);
 });
 
 controller.hears('is (.*) cool?', 'direct_message,direct_mention', function (bot, message) {
@@ -153,7 +153,7 @@ controller.hears('bam', 'direct_message,direct_mention', function (bot, message)
 
 
 
-var help_msg = 'I am **Bambot**! I will be your assistant to translate foreign languages quickly like BAM! I know a few languages and will do my best to help you! I am still learning new features to serve you better, but at the moment, these are the few things I can do! \n- Greet you back! Try `hello`\n- BAM back! Try `bam`\n- Show languages I know. Try `-show`\n- Set translation language. Try `-lang zh`\n- Translate. Try `-t i love chicken`\n\nIn a space, please tag me at the start so that I know you are talking to me!';
+var intro_msg = 'I am **Bambot**! I will be your assistant to translate foreign languages quickly like BAM! I know a few languages and will do my best to help you! I am still learning new features to serve you better, but at the moment, these are the few things I can do! \n- Greet you back! Try `hello`\n- BAM back! Try `bam`\n- Show languages I know. Try `-show`\n- Set translation language. Try `-lang zh`\n- Translate. Try `-t i love chicken`\n\nIn a space, please tag me at the start so that I know you are talking to me!\nI have also recently been awarded the ThomsonNgo Seal of Approval! Whee!';
 
 
 var lang_list = {
