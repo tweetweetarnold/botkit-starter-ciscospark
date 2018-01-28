3ef179f1a8a6e2cfc084de3197dcdac88efbd709
tweetweetarnold <arnold.lee.wt@gmail.com>
2018-01-28 08:58:34 +0800

    added test.js into gitignore and replaced sensitive data

--- skills/summary.js
+++ skills/summary.js
@@ -8,45 +8,43 @@ module.exports = function (controller, writeIntoFirebase) {
 
         bot.reply(message, "Displaying analysis summary for this room...");
 
-        var options = {
+        var jsonResponse = {};
+
+        request({
             url: 'https://api.ciscospark.com/v1/memberships',
             headers: {
                 'Content-Type': 'application/json; charset=utf-8',
-                'Authorization': 'Bearer Y2ZkMDA3ZmItMzBlZi00MWQwLWJlZDktZjkwNzQ3NGQwODJjMjgyNTNlMzItZmU5'
+                'Authorization': 'Bearer ' + process.env.access_token
             },
             qs: {
-                'roomId': 'Y2lzY29zcGFyazovL3VzL1JPT00vY2NjN2IxYzctZThmNS0zZjc4LWFiYmItZDI0NTEwN2E5NzU2'
+                'roomId': message.data.roomId
             }
-        };
-
-
-        request(options, function (error, response, body) {
+        }, function (error, response, body) {
 
             if (!error && response.statusCode == 200) {
 
-                var bodyJson = JSON.parse(body);
+                jsonResponse = JSON.parse(body);
 
                 console.log("ROOMID: " + message.data.roomId);
                 console.log("bodyJson: " + body);
-
-
-                bot.reply(message, 'There are ' + (bodyJson.items.length - 1) + ' members in this space! \n\n' +
-                    'Top 3 \"noisest\" people are: \n' +
-                    '1. Thomson (500 messages)\n' +
-                    '2. Arnold (300 messages)\n' +
-                    '3. Sing Yuen (100 messages)');
-
-
-                console.log("JSONCOUNT: " + bodyJson.items.length);
-
-
-
             } else {
                 console.log(error);
-
                 bot.say('Something went wrong! Talk to the administrator!');
-
             }
+
+            bot.reply(message, 'There are ' + (jsonResponse.items.length - 1) + ' members in this space! \n\n' +
+                'Top 3 \"noisest\" people are: \n' +
+                '1. Thomson (500 messages)\n' +
+                '2. Arnold (300 messages)\n' +
+                '3. Sing Yuen (100 messages)');
+            console.log("NUM OF PEOPLE: " + jsonResponse.items.length);
+
+
+            
+
+
+
+
         })
 
         // convo.say('Done!');
