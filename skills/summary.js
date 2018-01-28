1ff2a29f99f9e65931f9de19ff85ed848f20bdfd
tweetweetarnold <arnold.lee.wt@gmail.com>
2018-01-28 12:52:09 +0800

    completed summary for translation count

--- skills/summary.js
+++ skills/summary.js
@@ -2,7 +2,18 @@ const request = require('request');
 
 module.exports = function (controller, writeIntoFirebase, database) {
 
+
+
     controller.hears('-summary', 'direct_message,direct_mention', function (bot, message) {
+
+        var translateCountMapResult = function (map) {
+            console.log('translateCountMapResult...');
+            var returnString = "";
+            for (var myKey in map) {
+                returnString = returnString + "- <@personEmail:" + myKey + ">, count:" + map[myKey] + "\n";
+            }
+            bot.reply(message, returnString);
+        }
 
         bot.reply(message, "Displaying analysis summary for this room...");
 
@@ -24,7 +35,7 @@ module.exports = function (controller, writeIntoFirebase, database) {
                 jsonResponse = JSON.parse(body);
 
                 console.log("ROOMID: " + message.data.roomId);
-                console.log("bodyJson: " + body);
+                // console.log("bodyJson: " + body);
             } else {
                 console.log(error);
                 bot.say('Something went wrong! Talk to the administrator!');
@@ -35,13 +46,47 @@ module.exports = function (controller, writeIntoFirebase, database) {
 
 
             var translationCount = 0;
+            var translateCountMap = {};
             database.ref('history-translate').child('roomId=' + message.data.roomId).orderByKey()
                 .once('value').then(function (snapshot) {
-                    console.log("VALUE2: " + Object.keys(snapshot.val()).length);
-                    translationCount = Object.keys(snapshot.val()).length;
+                    // console.log("VALUE2: " + Object.keys(snapshot.val()).length);
+                    console.log("VALUE3: " + snapshot.numChildren());
+                    translationCount = snapshot.numChildren();
 
                     bot.reply(message, 'Translation has been carried out ' + translationCount + ' times!');
+
+
+                    snapshot.forEach(function (childSnapshot) {
+                        console.log('KEY: ' + childSnapshot.key);
+
+                        var childBody = childSnapshot.val();
+                        console.log('CHILD: ' + JSON.stringify(childBody));
+
+                        console.log("personEmail: " + childBody.personEmail);
+
+                        console.log("exists : " + translateCountMap[childBody.personEmail]);
+
+                        if (translateCountMap[childBody.personEmail] == undefined) {
+                            translateCountMap[childBody.personEmail] = 1;
+                        } else {
+                            translateCountMap[childBody.personEmail] = translateCountMap[childBody.personEmail] + 1;
+                        }
+
+                        // translateCountMap[childBody.personEmail]++;
+                        // console.log('TRANSLATE COUNT: ' + translateCountMap);
+                        console.log('TRANSLATE COUNT2: ' + JSON.stringify(translateCountMap));
+                        // bot.reply(message, JSON.stringify(translateCountMap));
+
+
+                    })
+
+                    translateCountMapResult(translateCountMap);
+                    // bot.reply(message, translateCountMap);
+                    // bot.reply(message, 'done');
+
                 })
+
+
 
 
         })
