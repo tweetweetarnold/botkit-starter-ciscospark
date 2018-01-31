3a5d4a015b6866bd8c7877d782103b9ff8f8e57a
Arnold Lee <arnold.lee.wt@gmail.com>
2018-01-31 16:38:34 +0800

    update points

--- skills/points.js
+++ skills/points.js
@@ -5,42 +5,94 @@ module.exports = function (controller, writeIntoFirebase, database) {
     controller.hears(['-points *'], 'direct_message,direct_mention', function (bot, message) {
 
         var addOrMinus = message.text.charAt(message.text.length - 1);
+        // message.text.substr(message.text)
+        var arr = message.text.toString().split(" ");
+        console.log("arr: " + arr[2]);
+        addOrMinus = arr[2];
+        var startIndex = message.text.indexOf("#") + 1;
+        console.log("STARTINDEX: " + startIndex);
+        if (startIndex == 0) {
+            bot.reply(message, "Did not find reasoning. Terminating...");
+            return;
+        }
+        var thisReason = message.text.substr(startIndex, message.text.length - 1);
+
+        console.log("REASON: " + thisReason);
 
         var taggedPeople = message.data.mentionedPeople;
 
         taggedPeople.forEach(function (personId) {
 
-            database.ref('points').child('personId=' + personId).once('value').then(function (snapshot) {
-                var personPoints = 0;
-                if (snapshot.val() != null) {
-                    personPoints = snapshot.val().points;
-                }
+            if (personId != 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS82NTAzYzgwNC1lMDJhLTRhMGYtYjczYi02NDc2NThiNmNjYzk') {
+                var personRef = database.ref('points').child('personId=' + personId);
 
-                if (personPoints === null) {
-                    personPoints = 0;
-                }
+                personRef.once('value').then(function (snapshot) {
+                    var personPoints = 0;
+                    if (snapshot.val() != null) {
+                        personPoints = snapshot.val().points;
+                    }
 
-                if (addOrMinus === "+") {
-                    database.ref('points').child('personId=' + personId).set({
-                        points: personPoints + 1
-                    })
-                } else if (addOrMinus === "-") {
-                    database.ref('points').child('personId=' + personId).set({
-                        points: personPoints - 1
-                    })
-                }
+                    if (personPoints === null) {
+                        personPoints = 0;
+                    }
 
-            })
+                    if (addOrMinus === "+") {
+                        personRef.update({
+                            points: personPoints + 1
+                        })
+                        personRef.child('reasons').push({
+                            add: true,
+                            reason: thisReason
+                        })
+
+                    } else if (addOrMinus === "-") {
+                        personRef.update({
+                            points: personPoints - 1
+                        })
+                        personRef.child('reasons').push({
+                            add: false,
+                            reason: thisReason
+                        })
+                    }
+
+                })
+            }
 
         });
 
+    })
 
+    controller.hears(['-reasons *'], 'direct_message,direct_mention', function (bot, message) {
+
+        var taggedPeople = message.data.mentionedPeople;
+
+        taggedPeople.forEach(function (personId) {
+            if (personId != 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS82NTAzYzgwNC1lMDJhLTRhMGYtYjczYi02NDc2NThiNmNjYzk') {
+                var personReasonRef = database.ref('points').child('personId=' + personId).child('reasons');
+                // personRef.child('points');
+
+                console.log("ID: " + personId);
+                personReasonRef.orderByKey().limitToLast(3).once('value').then(function (snapshot) {
+                    console.log("LAST3: " + JSON.stringify(snapshot.val()));
+
+
+
+
+
+
+                })
+
+
+            }
+        })
 
     })
 
+
+
     controller.hears(['-display'], 'direct_message,direct_mention', function (bot, message) {
 
-        database.ref('points').once('value').then(function (snapshot) {
+        database.ref('points').orderByValue().once('value').then(function (snapshot) {
 
             var returnString = "";
 
